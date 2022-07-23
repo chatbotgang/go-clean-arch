1ec676e57c529b5aecddcdc014ce2b741a0184f9
JalexChang <jalex.cpc@gmail.com>
2022-07-24 00:48:25 +0800

    feat(service): add method to register a trader

---
+++ internal/app/service/barter/interface.go
@@ -0,0 +1,19 @@
+package barter
+
+import (
+	"context"
+
+	"github.com/chatbotgang/go-clean-architecture-template/internal/domain/barter"
+	"github.com/chatbotgang/go-clean-architecture-template/internal/domain/common"
+)
+
+//go:generate mockgen -destination automock/auth_server.go -package=automock . AuthServer
+type AuthServer interface {
+	RegisterAccount(ctx context.Context, email string, password string) (string, common.Error)
+}
+
+//go:generate mockgen -destination automock/trader_repository.go -package=automock . TraderRepository
+type TraderRepository interface {
+	GetTraderByEmail(ctx context.Context, email string) (*barter.Trader, common.Error)
+	CreateTrader(ctx context.Context, trader barter.Trader) (*barter.Trader, common.Error)
+}
