c59ecc2b5e96b15a33d6610e3dc0202a31a1eb94
JalexChang <jalex.cpc@gmail.com>
2022-07-24 01:30:12 +0800

    feat(repository): add test cases for trader repo

---
+++ testdata/init_local_dev.sql
@@ -0,0 +1,6 @@
+begin;
+
+insert into trader ( uid, email, name)
+values ('c369b9e594fcd07c8e74cac1', 'trader1@cresclab.com', 'trader1');
+
+end;
---
+++ testdata/testdata.go
@@ -0,0 +1,21 @@
+package testdata
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+var basepath string
+
+const (
+	TestDataTrader = "trader.yml"
+)
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	basepath = filepath.Dir(currentFile)
+}
+
+func Path(rel string) string {
+	return filepath.Join(basepath, rel)
+}
